4d2f2d62a2ab789b51c839a24b0aed356b7f5ef3
Gustavo <gustavo@MacBook-Pro-de-Gustavo.local>
2017-12-03 11:16:20 -0200

    add route v1

--- routes/event_confirmation.js
+++ routes/event_confirmation.js
@@ -6,4 +6,9 @@ module.exports = (app) => {
         .post('/events', EventConfirmationController.create)
         .put('/events', EventConfirmationController.create)
         .get('/event/:id', EventConfirmationController.findOne)
+        .get('/v1/events', EventConfirmationController.find)
+        .post('/v1/events', EventConfirmationController.create)
+        .put('/v1/events', EventConfirmationController.create)
+        .get('/v1/event/:id', EventConfirmationController.findOne)
+    ;
 }
